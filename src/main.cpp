52e77691327f7aeb4ce9abdf36e333ccacc402e6
Andrew <andrew@infinityimpossible.com>
2021-04-27 00:59:37 +0100

    Support HomeAssistant device discovery
    Support homeassistant device discovery by publishing to the documented mqtt topic.
    Deep sleep for 30 mins rather than 20.
    Rescale results into 0. to 100. range up from 0. to 1.
    Juggle the sleeping loop so that we now wait for 2 seconds before disconnecting wifi instead of half a second afterwards.  Fixes mqtt publish not being sent.

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 
 #include "device_id.h"
 #include "mqtt.h"
-#include "wifi.h"
+#include "wifisetup.h"
 
 const int DHT_PIN = 22;
 const int DHT_TYPE = DHT11;
@@ -17,7 +17,7 @@ const int INFO_LED_PIN = 16;
 
 const String TOPIC_PREFIX = "higrow_plant_monitor/";
 const String TOPIC_SUFFIX = "/state";
-const int DEEP_SLEEP_SECONDS = 60 * 20;
+const int DEEP_SLEEP_SECONDS = 60 * 30;
 
 const int WATER_MEASUREMENTS = 10;
 const int WATER_MEASURE_INTERVAL_MS = 200;
@@ -34,6 +34,23 @@ String topic;
 WiFiClient wifi_client;
 PubSubClient mqtt_client(wifi_client);
 
+struct DiscoveryDevices
+{
+  char const* m_deviceClass;
+  char const* m_nameStrSuffix;
+  char const* m_uniqueIDSuffix;
+  char const* m_unitOfMeasurement;
+  char const* m_valueTemplate;
+  char const* m_icon;
+};
+
+DiscoveryDevices discoveryDevices[4] = {
+  {"temperature", " temperature", "_temperature", "°C", "{{ value_json.temperature_celsius }}", nullptr},
+  {"humidity", " humidity", "_humidity", "%", "{{ value_json.humidity_percent }}", nullptr},
+  {nullptr, " water", "_water", "%", "{{ value_json.water }}", "mdi:water-percent"},
+  {nullptr, " light", "_light", "%", "{{ value_json.light }}", "mdi:theme-light-dark"},
+};
+
 void print_info() {
   Serial.println(F("------------------------------------------------------------"));
   Serial.print(F("Device id:      "));
@@ -42,11 +59,90 @@ void print_info() {
   Serial.print(MQTT_BROKER_HOST);
   Serial.print(F(":"));
   Serial.println(MQTT_BROKER_PORT);
+  #ifdef MQTT_BROKER_USER
+  Serial.print(F("MQTT Username:  "));
+  Serial.println(MQTT_BROKER_USER);
+  #endif
+  #ifdef MQTT_BROKER_PASS
+  Serial.print(F("MQTT Password:  "));
+  Serial.println(MQTT_BROKER_PASS);
+  #endif
   Serial.print(F("Mqtt topic:     "));
   Serial.println(topic);
   Serial.print(F("Mqtt client id: "));
   Serial.println(client_id);
   Serial.println(F("------------------------------------------------------------"));
+}
+
+void addDeviceElement(DynamicJsonDocument& i_document)
+{
+  JsonObject deviceObject = i_document.createNestedObject("device");
+  JsonArray identifiersArray = deviceObject.createNestedArray("identifiers");
+  identifiersArray.add(client_id.c_str());
+  deviceObject["manufacturer"] = "DIY More";
+  deviceObject["model"] = "ESP32 DHT11 WIFI Bluetooth Soil Temperature Humidity Sensor18650";
+  deviceObject["name"] = client_id.c_str();
+  deviceObject["sw_version"] = "https://github.com/BubbaFatAss/higrow-mqtt-sender";
+}
+
+void sendMQTTDiscoveryTopic() {
+  if(reconnect_mqtt(mqtt_client, client_id.c_str())) {
+    uint const numDevices = sizeof(discoveryDevices) / sizeof(discoveryDevices[0]);
+    Serial.print("Starting HomeAssistnt discovery topics for ");
+    Serial.print(numDevices);
+    Serial.println(" devices...");
+    for (uint i = 0; i < numDevices; ++i)
+    {
+      DynamicJsonDocument configDocument(1024);
+      addDeviceElement(configDocument);
+      if( discoveryDevices[i].m_deviceClass != nullptr )
+      {
+        configDocument["device_class"] = discoveryDevices[i].m_deviceClass;
+      }
+      configDocument["json_attributes_topic"] = topic.c_str();
+      String nameStr = client_id + discoveryDevices[i].m_nameStrSuffix;
+      configDocument["name"] = nameStr.c_str();
+      //configDocument["state_topic"] = topic.c_str();
+      String uniqueID = client_id + discoveryDevices[i].m_uniqueIDSuffix;
+      configDocument["unique_id"] = uniqueID.c_str();
+      configDocument["unit_of_measurement"] = discoveryDevices[i].m_unitOfMeasurement;
+      configDocument["value_template"] = discoveryDevices[i].m_valueTemplate;
+      if( discoveryDevices[i].m_icon != nullptr )
+      {
+        configDocument["icon"] = discoveryDevices[i].m_icon;
+      }
+
+      String payload;
+      serializeJson(configDocument, payload);
+      String discoveryTopic = "homeassistant/sensor/";
+      discoveryTopic += client_id;
+      discoveryTopic += "/";
+      String nameStrTrimmed = discoveryDevices[i].m_nameStrSuffix;
+      nameStrTrimmed.trim();
+      discoveryTopic += nameStrTrimmed;
+      discoveryTopic += "/config";
+      if (mqtt_client.beginPublish(discoveryTopic.c_str(), payload.length(), true)) {
+        mqtt_client.print(payload);
+        if( mqtt_client.endPublish() == 1 )
+        {
+          Serial.print("Published discovery topic '");
+          Serial.print(discoveryTopic.c_str());
+          Serial.println("' successfully.");
+        } else {
+          Serial.print("endPublish discovery topic '");
+          Serial.print(discoveryTopic.c_str());
+          Serial.println("' failed.");
+        }
+      } else {
+        Serial.print("beginPublish discovery topic '");
+        Serial.print(discoveryTopic.c_str());
+        Serial.println("' failed.");
+      }
+    }
+    mqtt_client.disconnect();
+  } else {
+    Serial.println("Failed to connect MQTT Client, skipping HomeAssistant discovery topics");
+  }
 }
 
 void setup() {
@@ -68,13 +164,16 @@ void setup() {
 
   dht.begin();
 
+  //Send Homeassistant Device Discovery topic
+  sendMQTTDiscoveryTopic();
+
   digitalWrite(INFO_LED_PIN, LOW);
 }
 
 float read_sensor(int pin, int zero_value, int one_value) {
   int raw = analogRead(pin);
-  float level = map(raw, zero_value, one_value, 0, 10000) / 10000.;
-  return constrain(level, 0., 1.);
+  float level = map(raw, zero_value, one_value, 0, 10000) / 100.;
+  return constrain(level, 0., 100.);
 }
 
 float read_water_smoothed() {
@@ -106,14 +205,22 @@ void read_and_send_data() {
   root["water"] = read_water_smoothed();
   root["light"] = read_sensor(LIGHT_PIN, 0, 4095);
   Serial.print(F("Sending payload: "));
-  serializeJson(root, Serial);
-  Serial.println("");
+  String payload;
+  serializeJson(root, payload);
+  Serial.println(payload.c_str());
   if(reconnect_mqtt(mqtt_client, client_id.c_str())) {
-    String payload;
-    serializeJson(root, payload);
     if (mqtt_client.beginPublish(topic.c_str(), payload.length(), true)) {
       mqtt_client.print(payload);
-      mqtt_client.endPublish();
+      if( mqtt_client.endPublish() == 1 )
+      {
+        Serial.print("Published data topic '");
+        Serial.print(topic.c_str());
+        Serial.println("' successfully.");
+      } else {
+        Serial.print("endPublish data topic '");
+        Serial.print(topic.c_str());
+        Serial.println("' failed.");
+      }
       mqtt_client.disconnect();
       Serial.println("Success sending message");
     } else {
@@ -126,9 +233,9 @@ void loop() {
   read_and_send_data();
   Serial.println(F("Flushing wifi client"));
   wifi_client.flush();
+  delay(2000);  // Wait a bit to ensure message sending is complete
   Serial.println(F("Disconnecting wifi"));
   WiFi.disconnect(true, false);
-  delay(500);  // Wait a bit to ensure message sending is complete
   Serial.print(F("Going to sleep after "));
   Serial.print(millis());
   Serial.println(F("ms"));
